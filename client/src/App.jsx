3332121fe0211992204f41dfef0275abb3a5b326
Sushant <panditsushant11@gmail.com>
2025-05-23 17:39:40 +0530

    add code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import History from "./pages/History";
-import Footer from "./components/Footer";
+import React from 'react'
 
-function App() {
+function hello() {
   return (
-    <>
-    <ThemeProvider>
-      <Router>
-        <div className="min-h-screen bg-gradient-to-br from-slate-50 to-slate-100 dark:from-slate-900 dark:to-slate-800 transition-colors duration-300">
-          <Navbar />
-          <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/history" element={<History />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </ThemeProvider>
-    </>
-  );
+    <div>
+      hello
+    </div>
+  )
 }
 
-export default App;
+export default hello
